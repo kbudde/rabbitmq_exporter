b092225132a169c73ddae0294326aadf272c8667
David Ohayon <david.ohayon@traderepublic.com>
2023-07-19 17:13:44 +0200

    adding channel metrics

---
+++ exporter_channels.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func init() {
+	RegisterExporter("channels", newExporterChannels)
+}
+
+var (
+	channelLabelsStateMetric = []string{"cluster", "vhost", "node", "name", "user", "state", "confirm", "transactional", "self"}
+	channelLabelKeys         = []string{"vhost", "node", "name", "user", "state", "confirm", "transactional", "node"}
+)
+
+type exporterChannels struct {
+	stateMetric *prometheus.GaugeVec
+}
+
+func newExporterChannels() Exporter {
+	return exporterChannels{
+		stateMetric: newGaugeVec("channel_status", "Number of channels in a certain state aggregated per label combination.", channelLabelsStateMetric),
+	}
+}
+
+func (e exporterChannels) Collect(ctx context.Context, ch chan<- prometheus.Metric) error {
+	rabbitChannelResponses, err := getStatsInfo(config, "channels", channelLabelKeys)
+
+	if err != nil {
+		return err
+	}
+
+	e.stateMetric.Reset()
+
+	selfNode := ""
+	if n, ok := ctx.Value(nodeName).(string); ok {
+		selfNode = n
+	}
+	cluster := ""
+	if n, ok := ctx.Value(clusterName).(string); ok {
+		cluster = n
+	}
+
+	for _, connD := range rabbitChannelResponses {
+		self := selfLabel(config, connD.labels["node"] == selfNode)
+		e.stateMetric.WithLabelValues(
+			cluster,
+			connD.labels["vhost"],
+			connD.labels["node"],
+			connD.labels["name"],
+			connD.labels["user"],
+			connD.labels["state"],
+			connD.labels["confirm"],
+			connD.labels["transactional"],
+			self).Add(1)
+	}
+
+	e.stateMetric.Collect(ch)
+	return nil
+}
+
+func (e exporterChannels) Describe(ch chan<- *prometheus.Desc) {
+	// not implemented given that currently this exporter only exposes channel state metric
+}
